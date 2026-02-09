f6f52742cffb8e6ccf87a0e693a9fa5618ceb556
Mobi-hn <mobinahnnezhad@gmail.com>
2026-02-09 15:48:01 +0330

    Update stack.py

--- Data structure/project 4/stack.py
+++ Data structure/project 4/stack.py
@@ -1,13 +1,16 @@
 class Stack :
-    def __init__(self):
-        self.items = []
+    def __init__(self , size):
+        self.size = size
+        self.stack = [None]*size
+        self.top = -1
 
 
 def peek(self):
     if not self.is_empty():
-        return self.items[-1]
+        return self.stack[self.top]
     else:
         raise IndexError("stack is empty")
 
 def is_empty(self):
     return self.top == -1
+
