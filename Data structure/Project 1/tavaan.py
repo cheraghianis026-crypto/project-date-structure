c0d0be7997f76ce56b896fff2860fe3bf6b84a67
Mobi-hn <mobinahnnezhad@gmail.com>
2026-02-09 15:14:49 +0330

    Update tavaan.py

--- Data structure/Project 1/tavaan.py
+++ Data structure/Project 1/tavaan.py
@@ -1,8 +1,11 @@
 def tavan(n,m):
     if(m==1):
         return n
+    elif (m==0):
+        return 1
     else:
         return n*tavan(n,m-1)
 n = int(input())
 m = int(input())
+
 print (tavan(n,m))
